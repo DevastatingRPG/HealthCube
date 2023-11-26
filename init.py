61a5a04039d08f74db40a044203f98fd70ed5fa3
DevastatingRPG <devastatingrpg@gmail.com>
2023-11-26 12:38:44 +0530

    changed init.py

--- init.py
+++ init.py
@@ -5,7 +5,6 @@ sql_user = input("Enter SQL user: ")
 sql_password = input("Enter SQL password: ")
 sql_database = input("Enter SQL database: ")
 sql_server = input("Enter SQL server: ")
-jwt_secret_key = input("Enter JWT secret key: ")
 ipv4 = input("Enter ipv4 address of device used to run node server : ")
 
 # Write the input to the .env file
@@ -14,7 +13,7 @@ with open('server/.env', 'w') as f:
    f.write(f'SQL_PASSWORD = "{sql_password}"\n')
    f.write(f'SQL_DATABASE = "{sql_database}"\n')
    f.write(f'SQL_SERVER = "{sql_server}"\n')
-   f.write(f'JWT_SECRET_KEY = "{jwt_secret_key}"\n')
+   f.write(f'JWT_SECRET_KEY = \"2ea0e8ab7067ef90bfbc9d319892491d177403df4467ab7059149e3f80af97ec\"\n')
 
 with open('client/.env', 'w') as f:
    f.write(f'EXPO_PUBLIC_IPv4 = "{ipv4}"')
